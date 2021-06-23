6307a760a592638f66090a840e5b5f2a21271a22
Pedro Matos <pedro.matos@upflow.me>
2021-06-23 14:57:46 -0300

    Created useAuth context.

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import { BrowserRouter, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 
+import { AuthContextProvider } from "./contexts/AuthContexts";
+
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Home} />
-      <Route path="/rooms/new" component={NewRoom} />
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
